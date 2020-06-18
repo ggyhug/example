d1f9356b14f4e82951792d8f496c8c9ff8671d82
guogy <50547999+ggyhug@users.noreply.github.com>
2020-06-18 12:57:07 +0800

    Update metrics.go

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -2,8 +2,30 @@ package metrics
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
+        "encoding/json"
+	"log"
+	"metrics-server-exporter-go/api"
 	"time"
 )
+
+// Info - Data structure of pod
+type Info struct {
+	Items []struct {
+		Metadata struct {
+			Name              string    `json:"name"`
+			Namespace         string    `json:"namespace"`
+			CreationTimestamp time.Time `json:"creationTimestamp"`
+		} `json:"metadata"`
+		Containers []struct {
+			Name  string `json:"name"`
+			Usage struct {
+				CPU    string `json:"cpu"`
+				Memory string `json:"memory"`
+			} `json:"usage"`
+		} `json:"containers"`
+	} `json:"items"`
+}
+
 
 var (
 	requestCount = prometheus.NewCounterVec(
@@ -20,6 +42,23 @@ var (
 			Buckets:   []float64{0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0, 60.0, 120.0, 300.0},
 		}, []string{},
 	)
+	
+	// MetricsPodsCPU - CPU Gauge
+	MetricsPodsCPU = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "kube_metrics_server_pods_cpu",
+			Help: "Metrics Server Pods CPU",
+		},
+		[]string{"pod_name", "pod_namespace", "pod_container_name"},
+	)
+	// MetricsPodsMEM - Memory Gauge
+	MetricsPodsMEM = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "kube_metrics_server_pods_mem",
+			Help: "Metrics Server Pods Memory",
+		},
+		[]string{"pod_name", "pod_namespace", "pod_container_name"},
+	)
 )
 
 // AdmissionLatency measures latency / execution time of Admission Control execution
@@ -32,6 +71,8 @@ type RequestLatency struct {
 func Register() {
 	prometheus.MustRegister(requestCount)
 	prometheus.MustRegister(requestLatency)
+	prometheus.MustRegister(MetricsPodsMeM)
+	prometheus.MustRegister(MetricsPodsCPU)
 }
 
 
@@ -52,4 +93,24 @@ func (t *RequestLatency) Observe() {
 // RequestIncrease increases the counter of request handled by this service
 func RequestIncrease() {
 	requestCount.WithLabelValues().Add(1)
+	var pods Info
+	log.Println("Starting collect POD data,")
+
+	apiPod := api.Connect("pod")
+
+	_ = json.NewDecoder(apiPod.Body).Decode(&pods)
+
+	for i := range pods.Items {
+
+		podName := pods.Items[i].Metadata.Name
+		podNamespace := pods.Items[i].Metadata.Namespace
+
+		for j := range pods.Items[i].Containers {
+
+			MetricsPodsCPU.With(prometheus.Labels{"pod_name": podName, "pod_namespace": podNamespace, "pod_container_name": pods.Items[i].Containers[j].Name}).Add(api.ReturnFloat(pods.Items[i].Containers[j].Usage.CPU))
+			MetricsPodsMEM.With(prometheus.Labels{"pod_name": podName, "pod_namespace": podNamespace, "pod_container_name": pods.Items[i].Containers[j].Name}).Add(api.ReturnFloat(pods.Items[i].Containers[j].Usage.Memory))
+		}
+
+	}
+	log.Println("POD data collected.")
 }
